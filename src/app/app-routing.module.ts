ead668b8665e5305eac8ba8ffe0ded7216c3a4dc
Rafagg <rafael.gomez@gmail.com>
2018-03-24 10:05:43 +0100

    Feature authentication implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { AuthModule } from './auth/auth.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/containers/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), AuthModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
