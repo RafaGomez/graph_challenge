0e8bba786f1e75f05520f91ab755ea2497776bdf
Rafa <r.gomez@city-ss.com>
2018-04-16 14:19:21 +0200

    fixing routing issue with graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { GraphContainerComponent } from './graph/containers/graph-container/graph-container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GraphContainerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
